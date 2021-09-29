418ec3c4ff09dd5f8edff11272444ad972d4472b
Tay-Zhan-An <1801016@sit.singaporetech.edu.sg>
2021-09-29 21:01:44 +0800

    Bug fix 2

--- src/module/user/apiService.js
+++ src/module/user/apiService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const baseURL = 'http://springboot-env.eba-qfbr9n2v.ap-southeast-1.elasticbeanstalk.com';
+// const baseURL = 'http://springboot-env.eba-qfbr9n2v.ap-southeast-1.elasticbeanstalk.com';
+
+const baseURL = 'https://www.hackathon2021.tk/';
+
 //const baseURL = 'http://localhost:5000'
 
 export const loginService = async (params) => {
